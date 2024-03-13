f58b7582e25b9845a610fce3872986b261237ac4
Matt Williamson <matt.williamson@cdw.com>
2024-03-13 15:09:27 -0500

    Moving to secrets instead of variables

--- ansible_action/terraform/main.tf
+++ ansible_action/terraform/main.tf
@@ -9,29 +9,29 @@ resource "github_repository" "this" {
 
 }
 
-# resource "github_actions_secret" "public_ip" {
-#   repository       = github_repository.this.name
-#   secret_name      = "public_ip"
-#   plaintext_value  = module.aws.public_ip
-# }
-
-# resource "github_actions_secret" "ssh_private_key" {
-#   repository       = github_repository.this.name
-#   secret_name      = "SSH_PRIVATE_KEY"
-#   plaintext_value  = file("~/.ssh/id_ed25519")
-# }
-
-resource "github_actions_variable" "public_ip" {
+resource "github_actions_secret" "public_ip" {
   repository       = github_repository.this.name
-  variable_name    = "PUBLIC_IP"
-  value            = module.aws.public_ip
+  secret_name      = "public_ip"
+  plaintext_value  = module.aws.public_ip
 }
 
-resource "github_actions_variable" "ssh_private_key" {
+resource "github_actions_secret" "ssh_private_key" {
   repository       = github_repository.this.name
-  variable_name    = "SSH_PRIVATE_KEY"
-  value            = file("~/.ssh/id_ed25519")
+  secret_name      = "SSH_PRIVATE_KEY"
+  plaintext_value  = file("~/.ssh/id_ed25519")
 }
+
+# resource "github_actions_variable" "public_ip" {
+#   repository       = github_repository.this.name
+#   variable_name    = "PUBLIC_IP"
+#   value            = module.aws.public_ip
+# }
+
+# resource "github_actions_variable" "ssh_private_key" {
+#   repository       = github_repository.this.name
+#   variable_name    = "SSH_PRIVATE_KEY"
+#   value            = file("~/.ssh/id_ed25519")
+# }
 
 module "aws" {
   source = "./aws"
